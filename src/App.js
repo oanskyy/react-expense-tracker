53562245d8fe1031d8aa4f33dec2491e6d78edb7
Oana <oana@pectusfinance.com>
2022-06-26 01:24:35 +0200

    Split components into multiple components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import ExpenseItem from "./components/ExpenseItem";
-import { expenses } from "./Expenses";
+import Expenses from "./components/Expenses";
+import { expenses } from "./Data";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
+      {/* <Expenses
+        date={expenses.date}
+        title={expenses.title}
+        amount={expenses.amount}
+      /> */}
+      <Expenses expenses={expenses} />
     </div>
   );
 }
