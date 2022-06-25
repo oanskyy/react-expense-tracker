fca24b7469a7d69312c13d21cbcc9984bd615a75
Oana <oana@pectusfinance.com>
2022-06-25 02:48:37 +0200

    Pass data via Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 6, 25);
-  const expenseTitle = "Rent";
-  const expenseAmount = 1.2;
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2022, 6, 25);
+  // const expenseTitle = "Rent";
+  // const expenseAmount = 1.2;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
