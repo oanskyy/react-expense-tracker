a59ca7908d3ad0bddc544745a29d7963049a02bf
Oana <oana@pectusfinance.com>
2022-06-26 11:24:14 +0200

    Use one state object instead of multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,23 +2,41 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [title, setTitle] = useState('');
-  const [amount, setAmount] = useState('');
+  //   const [title, setTitle] = useState('');
+  //   const [amount, setAmount] = useState('');
+  //   const [date, setDate] = useState('');
+
+  //   const titleChangeHandler = (event) => {
+  //     setTitle(event.target.value);
+  //     console.log(event.target.value);
+  //     console.log(title);
+  //   };
+
+  //   const amountChangeHandler = (event) => {
+  //     setAmount(event.target.value);
+  //     console.log(event.target.value);
+  //     console.log(amount);
+  //   };
+
+  //   const dateChangeHandler = (event) => {
+  //     console.log(event.target.value);
+  //     setUserInput({
+  //       ...userInput,
+  //       title: event.target.value,
+  //     });
+  //   };
+
+  const [userInput, setUserInput] = useState({
+    title: "",
+    amount: "",
+    date: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setTitle(event.target.value);
-    console.log(event.target.value);
-    console.log(title)
-  };
-
-  const amountChangeHandler = (event) => {
-    setAmount(event.target.value);
-    console.log(event.target.value);
-    console.log(amount)
-  };
-
-  const dateChangeHandler = (event) => {
-    console.log(event.target.value);
+    setUserInput({
+      ...userInput,
+      title: event.target.value,
+    });
   };
 
   return (
@@ -34,7 +52,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
-            onChange={amountChangeHandler}
+            // onChange={amountChangeHandler}
           />
         </div>
         <div className="new-expense__control">
@@ -43,7 +61,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019.01.01"
             max="2022.04.01"
-            onChange={dateChangeHandler}
+            // onChange={dateChangeHandler}
           />
         </div>
       </div>
