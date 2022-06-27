4ca752d1c167bac6c6d7152e41eb797e0febe5aa
oanskyy <oana.anghelachi@gmail.com>
2022-06-27 21:13:52 +0200

    Add dynamic styles

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -10,9 +10,10 @@ const ChartBar = (props) => {
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill" style={{ height: barFillHeight }}>
-          {props.value}
-        </div>
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
     </div>
