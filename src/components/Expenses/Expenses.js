91f734f87475117ab46af15243aabaf230c1047f
oanskyy <oana.anghelachi@gmail.com>
2022-06-27 09:21:19 +0200

    Dynamically render lists of data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,35 +7,22 @@ import { useState } from "react";
 const Expenses = (props) => {
   const [year, setYear] = useState("2020");
 
-  const filterChangeHandler = (selectedYear) => { 
-    console.log(selectedYear)
+  const filterChangeHandler = (selectedYear) => {
+    console.log(selectedYear);
     setYear(selectedYear);
-  }
+  };
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={year} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          date={props.items[0].date}
-          amount={props.items[0].amount}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          date={props.items[1].date}
-          amount={props.items[1].amount}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          date={props.items[2].date}
-          amount={props.items[2].amount}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          date={props.items[3].date}
-          amount={props.items[3].amount}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        ))}
       </Card>
     </div>
   );
