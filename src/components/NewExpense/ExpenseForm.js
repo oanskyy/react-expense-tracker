bb0b23eefe5dd10566c018cd87ecc45b871ea196
oanskyy <oana.anghelachi@gmail.com>
2022-06-27 15:06:26 +0200

    Add conditional rendering for toggle newExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,6 +2,12 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleForm = () => {
+    setIsVisible(!isVisible);
+  };
+
   const [enteredTitle, setTitle] = useState(" ");
   const [enteredAmount, setAmount] = useState(" ");
   const [enteredDate, setDate] = useState(" ");
@@ -48,7 +54,7 @@ const ExpenseForm = (props) => {
       date: new Date(enteredDate),
     };
 
-    props.onSaveExpenseData(expenseData)
+    props.onSaveExpenseData(expenseData);
     console.log(expenseData);
     setTitle("");
     setAmount("");
@@ -56,41 +62,56 @@ const ExpenseForm = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+    <div>
+      {!isVisible && (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={enteredTitle}
+                onChange={titleChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={enteredAmount}
+                onChange={amountChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019.01.01"
+                max="2022.12.12"
+                value={enteredDate}
+                onChange={dateChangeHandler}
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type="button" onClick={toggleForm}>
+              Cancel
+            </button>
+            <button type="submit">Add expense</button>
+          </div>
+        </form>
+      )}
+
+      {isVisible && (
+        <div className="new-expense__actions">
+          <button type="button" onClick={toggleForm}>
+            Add New Expense
+          </button>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019.01.01"
-            max="2022.12.12"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add expense</button>
-      </div>
-    </form>
+      )}
+    </div>
   );
 };
 
